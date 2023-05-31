dc71053efe2fb4bc4c51eb4ae9a3147b07728490
Shaun Reilly <104159375+IronhackShaun@users.noreply.github.com>
2023-05-31 02:04:50 +0100

    Splashscreen Completed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, { webpack }) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,353 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import { useEffect, useRef, useState } from "react";
+import Matter, { Mouse, MouseConstraint } from "matter-js";
+import CharS from "../public/char-s.svg";
+import PointingHand from "../public/pointing-hand.svg";
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const canvasRef = useRef(null);
+  const devStrictModeFix = useRef(false);
+  let handOpacity = useRef(-1);
+  let animDelay = useRef(true);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  const marqueeLines = [
+    "React Developer",
+    "Angular Developer",
+    "Java Springboot",
+    "Tailwind CSS",
+    "NextJS",
+    "MongoDB, SQL, Firebase",
+    "Climber",
+    "Web-dev Teacher",
+    "Microservices",
+    "Unit Testing",
+    "That U is intentional",
+    "Git Workflow",
+  ];
+  const [currentLine, setCurrentLine] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const marqueeInterval = setInterval(() => {
+    let randomIndex = Math.floor(Math.random() * marqueeLines.length);
+    while (randomIndex === currentIndex) {
+      randomIndex = Math.floor(Math.random() * marqueeLines.length);
+    }
+    setCurrentLine(marqueeLines[randomIndex]);
+    setCurrentIndex(randomIndex);
+    animDelay.current = false;
+  }, 5000);
+
+  const handOpactiyInterval = setInterval(() => {
+    handOpacity.current = 1;
+  }, 1000);
+
+  useEffect(() => {
+    document.body.classList.add("hide-scrollbar");
+
+    if (devStrictModeFix.current == false) {
+      devStrictModeFix.current = true;
+      return;
+    }
+
+    const handleMouseWheel = (event) => {
+      // Get the deltaY value to determine the direction of the scroll
+      const deltaY = event.deltaY;
+
+      // Scroll the page based on the deltaY value
+      window.scrollBy({
+        top: deltaY,
+        behavior: "auto", // You can change this to "auto" for instant scrolling
+      });
+    };
+
+    window.addEventListener("wheel", handleMouseWheel);
+
+    function getXValueInRange(
+      rangeStart: number,
+      rangeEnd: number,
+      maxDivisions: number,
+      divisionIndex: number
+    ) {
+      const divisionSize = (rangeEnd - rangeStart) / maxDivisions;
+      const x = rangeStart + divisionIndex * divisionSize + divisionSize / 2;
+      return x;
+    }
+
+    function createTextObject(
+      text: string,
+      x: number,
+      y: number,
+      fontsize?: number
+    ) {
+      const fontSize = fontsize ? fontsize : 250;
+      const textElement = document.createElement("div");
+      textElement.style.position = "absolute";
+      textElement.style.left = `${x}px`;
+      textElement.style.top = `${y}px`;
+      textElement.style.fontSize = `${fontSize}px`;
+      textElement.style.color = "black";
+      textElement.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+      textElement.style.pointerEvents = "none"; // Set pointer-events to none
+      textElement.innerHTML = text;
+      if (text == "U") {
+        textElement.style.rotate = "0.4rad";
+      }
+      document.body.appendChild(textElement);
+
+      const width = textElement.clientWidth;
+      const height = textElement.clientHeight;
+      const body = Matter.Bodies.rectangle(x, y, width, height, {
+        isStatic: false,
+        render: { visible: false },
+        frictionAir: 0.08 + Math.random() * 0.06,
+        label: text,
+        inertia: Infinity,
+        collisionFilter: {
+          category: 0x0002, // Collides with everything except floor
+          mask: 0x0001, // Collides with floor
+        },
+      });
+
+      const updateTextPosition = () => {
+        let previousHeight = parseFloat(textElement.style.top);
+        const position = body.position;
+        textElement.style.left = `${position.x - width / 2}px`;
+        textElement.style.top = `${position.y - height / 2}px`;
+      };
+
+      Matter.Events.on(engine, "afterUpdate", updateTextPosition);
+
+      return { body, textElement };
+    }
+
+    // Rotation Version Kinda buggy
+    // function createTextObject(text: string, x: number, y: number) {
+    //   const fontSize = 250;
+    //   const textElement = document.createElement("div");
+    //   textElement.style.position = "absolute";
+    //   textElement.style.left = `${x}px`;
+    //   textElement.style.top = `${y}px`;
+    //   textElement.style.fontSize = `${fontSize}px`;
+    //   textElement.style.color = "black";
+    //   textElement.style.backgroundColor = "rgba(0, 0, 0, 0.0)";
+    //   textElement.innerHTML = text;
+    //   if (text == "U") {
+    //     textElement.style.rotate = "0.4rad";
+    //   }
+    //   document.body.appendChild(textElement);
+
+    //   const width = textElement.clientWidth;
+    //   const height = textElement.clientHeight;
+    //   const body = Matter.Bodies.rectangle(x, y, width, height * 0.8, {
+    //     isStatic: false,
+    //     render: { visible: true },
+    //     frictionAir: 0.1 - Math.random() * 0.05,
+    //     label: text,
+    //   });
+    //   if (text == "U") {
+    //     body.angle = 0.4;
+    //   }
+
+    //   const updateTextPosition = () => {
+    //     let previousHeight = parseFloat(textElement.style.top);
+    //     const position = body.position;
+    //     const angle = body.angle;
+    //     textElement.style.left = `${position.x - width / 2}px`;
+    //     textElement.style.top = `${position.y - height / 2}px`;
+    //     textElement.style.rotate = `${body.angle}rad`;
+    //     if (body.position.y > window.innerHeight / 2.2) {
+    //       body.isStatic = true;
+    //     }
+    //   };
+
+    //   Matter.Events.on(engine, "afterUpdate", updateTextPosition);
+
+    //   return { body, textElement };
+    // }
+
+    function createTextBodiesFromString(string: string) {
+      for (let i = 0; i < string.length; i++) {
+        const charXValue = getXValueInRange(
+          window.innerWidth / 10,
+          window.innerWidth - window.innerWidth / 10,
+          string.length,
+          i
+        );
+        const charText = createTextObject(string[i], charXValue, -300);
+        bodies.push(charText.body);
+      }
+    }
+
+    const canvas = canvasRef.current;
+    const engine = Matter.Engine.create();
+    const render = Matter.Render.create({
+      canvas: canvas,
+      engine: engine,
+      options: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        background: "transparent",
+        wireframeBackground: "transparent",
+        wireframes: false,
+      },
+    });
+
+    const bodies:
+      | any[]
+      | Matter.Body
+      | Matter.Composite
+      | Matter.Constraint
+      | Matter.MouseConstraint = [];
+
+    const floor = Matter.Bodies.rectangle(
+      window.innerWidth / 2,
+      window.innerHeight / 1.5,
+      window.innerWidth,
+      10,
+      { isStatic: true, render: { visible: false } }
+    );
+    const ceiling = Matter.Bodies.rectangle(
+      window.innerWidth / 2,
+      0,
+      window.innerWidth,
+      10,
+      {
+        isStatic: true,
+        render: { visible: false },
+        collisionFilter: {
+          category: 0x0002,
+          mask: 0x0000,
+        },
+      }
+    );
+    bodies.push(floor, ceiling);
+
+    function spawnLastNameWithDelay() {
+      setTimeout(() => {
+        const surnameBody = createTextObject(
+          "Reilly",
+          window.innerWidth * 0.9,
+          -100,
+          50
+        );
+        surnameBody.body.frictionAir = 0.01;
+        surnameBody.body.restitution = 0.4;
+        surnameBody.body.force = { x: -0.15, y: 0 };
+
+        // Set collision filter for surnameBody
+        surnameBody.body.collisionFilter = {
+          category: 0x0002, // Collides with floor
+          mask: 0x0001, // Collides with everything except floor
+        };
+
+        Matter.World.add(engine.world, surnameBody.body);
+      }, 7000); // Delay of 5000 milliseconds (5 seconds)
+    }
+
+    createTextBodiesFromString("SHAUN");
+    spawnLastNameWithDelay();
+
+    // Create constraints to chain the letters to the ceiling
+    const constraintOptions = {
+      stiffness: 0.0001,
+      damping: 0.01,
+      length: window.innerHeight / 10,
+    };
+
+    for (let i = 2; i < bodies.length; i++) {
+      const charXValue = getXValueInRange(
+        window.innerWidth / 10,
+        window.innerWidth - window.innerWidth / 10,
+        bodies.length - 2,
+        i
+      );
+      const attachPoint = () => {
+        let letter = bodies[i].label;
+        switch (letter) {
+          case "S":
+            return { x: 0, y: -55 };
+          case "H":
+            return { x: 0, y: 10 };
+          case "A":
+            return { x: 0, y: -55 };
+          // Rotation problems
+          case "U":
+            return { x: -45, y: -55 };
+          // case "U":
+          //   return { x: 0, y: 90 };
+          default:
+            return { x: 0, y: 0 };
+        }
+      };
+      const constraint = Matter.Constraint.create({
+        bodyA: ceiling,
+        pointA: { x: charXValue - window.innerWidth / 1.2, y: 0 },
+        bodyB: bodies[i],
+        pointB: attachPoint(),
+        render: {
+          type: "line",
+          anchors: false,
+          strokeStyle: `#875638`,
+          lineWidth: 7,
+        },
+        ...constraintOptions,
+      });
+
+      Matter.World.add(engine.world, constraint);
+    }
+
+    Matter.World.add(engine.world, bodies);
+
+    const mouse = Mouse.create(canvas);
+    const mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 0.2,
+        render: {
+          visible: false,
+        },
+      },
+    });
+
+    Matter.World.add(engine.world, mouseConstraint);
+
+    Matter.Runner.run(engine);
+    Matter.Render.run(render);
+
+    console.log("hello");
+
+    return () => {
+      Matter.Render.stop(render);
+      Matter.Engine.clear(engine);
+      clearInterval(marqueeInterval);
+      clearInterval(handOpactiyInterval);
+      window.removeEventListener("wheel", handleMouseWheel);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="bg-slate-800 flex flex-col h-screen overflow-hidden">
+        <div
+          className="canvas-container gradient-opacity h-screen bg-cover bg-no-repeat bg-center bg-fixed"
+          style={{
+            backgroundImage: " url(/climbingWall.png)",
+          }}
+        >
+          <canvas ref={canvasRef}></canvas>
+        </div>
+
+        <div className="marqueen-container absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 uppercase text-center flex flex-col">
+          <h2 className="text-3xl">{currentLine}</h2>
+        </div>
+        <PointingHand
+          opacity={handOpacity.current}
+          height={"5vh"}
+          className="absolute z-10 bottom-1.5 left-1/2 transform rotate-90 -translate-x-1/2 -translate-y-1/2 duration-200 transition-opacity animate-pulse"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <div className="h-screen bg-slate-800"></div>
+    </div>
+  );
 }
