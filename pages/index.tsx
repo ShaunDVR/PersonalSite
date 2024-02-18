610af9f3fd8c9e18e46127edd7ab77dacc533762
Shaun Reilly <104159375+IronhackShaun@users.noreply.github.com>
2024-02-18 15:47:17 +0000

    mobile loader size fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -472,7 +472,7 @@ export default function Home() {
   return (
     <>
       {isLoaded ? null : (
-        <div className="flex items-center justify-center h-screen bg-slate-800">
+        <div className="flex items-center justify-center h-screen w-screen bg-slate-800">
           <ClimbingBoxLoader color="#36d7b7" size={40} />
         </div>
       )}
