fa61dc72960b3dde4a8093b03bd1f3f2400c4d7b
Shaun Reilly <104159375+IronhackShaun@users.noreply.github.com>
2024-02-18 16:27:50 +0000

    bg color fix for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -489,10 +489,10 @@ export default function Home() {
   }, []);
 
   return (
-    <>
+    <div className="bg-slate-800">
       {isLoaded ? null : (
         <div className="flex items-center justify-center h-screen w-screen bg-slate-800">
-          <ClimbingBoxLoader color="#36d7b7" size={40} />
+          <ClimbingBoxLoader color="#36d7b7" size={60} />
         </div>
       )}
       <div className="bg-slate-800" hidden={!isLoaded}>
@@ -530,6 +530,6 @@ export default function Home() {
         </div>
         <AppFooter />
       </div>
-    </>
+    </div>
   );
 }
